e731a36615b129f13dde3d158b6a6509c9b0f809
Võ Chí Thông <thong.vc.65cntt@ntu.edu.vn>
2026-03-20 18:22:07 +0700

    BTH7

--- BaiTH7_ListView1/app/src/main/java/ntu/httt_65/listview/MainActivity.java
+++ BaiTH7_ListView1/app/src/main/java/ntu/httt_65/listview/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
-        //Giữ nguyên phần xử lý tràn viền
+        //Xử lý tràn viền
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
