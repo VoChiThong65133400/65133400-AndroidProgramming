fa9cdc4b1acfe553cf319eb2cf7344bb770357a5
Võ Chí Thông <thong.vc.65cntt@ntu.edu.vn>
2026-04-20 07:46:42 +0700

    COMMIT lần 2

--- Thigk2.VoChiThong_65133400/app/src/main/java/ntu/httt_65/thigk2vochithong/ChucNang1Activity.java
+++ Thigk2.VoChiThong_65133400/app/src/main/java/ntu/httt_65/thigk2vochithong/ChucNang1Activity.java
@@ -1,8 +1,6 @@
 package ntu.httt_65.thigk2vochithong;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,27 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-    Button btnCN1, btnCN2, btnCN3, btnCN4;
+public class ChucNang1Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        setContentView(R.layout.activity_chuc_nang1);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.CN1), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        btnCN1 = findViewById(R.id.btnCN1);
-        btnCN2 = findViewById(R.id.btnCN2);
-        btnCN3 = findViewById(R.id.btnCN3);
-        btnCN4 = findViewById(R.id.btnCN4);
-
-        
     }
-
 }
